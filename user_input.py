a56e1c902a6cee5364dddd2c4a688917456c3ca6
CleophusMaina <mainacleophus@gmail.com>
2024-08-18 17:13:22 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# Define variables for name, age, and location
+Name="Cleophus"
+Age= 18
+Location ="Wajir"
+
+# Print the personalized message
+print(f"Hello {Name}, you are {Age} years old and live in {Location}.")
